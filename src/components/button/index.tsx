48b5c7e0bbafd388faae4c056a0bcbbe80b8a15f
Pedro Varela <pedrao.varela@gmail.com>
2025-04-01 23:28:23 -0300

    feat: buttonIcon -> linkIcon

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,9 +1,14 @@
+import type { ComponentProps } from 'react';
 import styles from './styles.module.css';
 
-type ButtonIconProps = {
+type LinkIconProps = ComponentProps<'a'> & {
   children: React.ReactNode;
 };
 
-export function ButtonIcon({ children }: ButtonIconProps) {
-  return <button className={styles.button}>{children}</button>;
+export function LinkIcon({ children, ...props }: LinkIconProps) {
+  return (
+    <a {...props} className={styles.linkIcon}>
+      {children}
+    </a>
+  );
 }
