4d09fa653d424f6fd865afb6871f6843456c9bca
Pedro Varela <pedro.varela.dev@gmail.com>
2025-04-10 20:49:05 -0300

    feat: input

--- src/components/taskForm/index.tsx
+++ src/components/taskForm/index.tsx
@@ -2,12 +2,38 @@ import { Play } from 'lucide-react';
 import { Button } from '../button';
 import { Cycles } from '../cycles';
 import { DefaultInput } from '../input';
+import { useState, useRef } from 'react'
+
+import { TaskModel } from '../../models/task.model.ts'
 
 import styles from './styles.module.css';
 
 export function TaskForm() {
+  const taskInput = useRef<HTMLInputElement>(null)
+
   function handleCreateTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
+    console.log(taskInput.current.value)
+
+    if(taskInput.current === null) return;
+
+    const taskName = taskInput.current.value.trim()
+
+    if(!taskName) {
+      // Alerta Tostfy
+      alert('Digite o nome da tarefa!')
+      return;
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      duration: 1,
+      type: 'worktime'
+    }
   }
 
   return (
@@ -17,6 +43,8 @@ export function TaskForm() {
           id='taskpomodoro'
           labelText='Atividade'
           placeholder='O que você vai fazer?'
+
+          ref={taskInput}
         />
       </div>
 
